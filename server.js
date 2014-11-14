dfd67fa5ae1855747d0c2c2ba96778249cba3430
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-14 14:35:15 -0800

    updated server files file passport & jwt

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
+var passport = require('passport');
 var app = express();
 
-app.use(bodyparser.json());
-
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_development');
+app.use(bodyparser.json());
+app.set('jwtSecret', process.env.JWT_secret || 'changethisordie');
 
-require('./routes/notes_routes')(app);
+require('./routes/users_routes')(app, passport);
+require('./routes/notes_routes')(notesRouter);
+app.use('/v1', notesRouter);
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
