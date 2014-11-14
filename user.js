e6582d8138115f9e8f04fcd69ba202d0cd7db3ee
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-14 13:22:49 -0800

    added user model

---
+++ user.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var jwt = require('jwt-simple');
+
+var userSchema = mongoose.Schema({
+  basic: {
+    email: String,
+    password: String
+  }
+});
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.basic.password);
+};
+
+userSchema.methods.generateToken = function(secret) {
+  var self = this;
+  var token = jwt.encode({
+    iss: self._id
+  }, secret);
+  return token;
+};
+
+module.exports = mongoose.model('User', userSchema);
