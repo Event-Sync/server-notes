8035738a1ea22143aba0ea691edf463928edde44
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-17 11:38:17 -0800

    updated server

--- server.js
+++ server.js
@@ -8,11 +8,21 @@ mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_development
 app.use(bodyparser.json());
 app.set('jwtSecret', process.env.JWT_secret || 'changethisordie');
 
+app.use(passport.initialize());
+
+require('./lib/passport')(passport);
+var jwtauth = require('./lib/jwt_auth')(app.get('jwtSecret'));
+
+var notesRouter = express.Router();
+notesRouter.use(jwtauth);
+
+
+require('./routes/event_routes')(app);
 require('./routes/users_routes')(app, passport);
 require('./routes/notes_routes')(notesRouter);
 app.use('/v1', notesRouter);
 
 app.set('port', process.env.PORT || 3000);
+console.log('server running on port: %d', app.get('port'));
 app.listen(app.get('port'), function() {
-  console.log('server running on port: %d', app.get('port'));
 });
