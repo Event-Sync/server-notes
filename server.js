192f83d74ce11aa4f31977cbb1ea61f68d3192ec
Jacob Shafer <jacshfr@gmail.com>
2014-11-17 12:04:06 -0800

    make test environment

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ var bodyparser = require('body-parser');
 var passport = require('passport');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_development');
 app.use(bodyparser.json());
 app.set('jwtSecret', process.env.JWT_secret || 'changethisordie');
 
-require('./routes/users_routes')(app, passport);
-require('./routes/notes_routes')(notesRouter);
-app.use('/v1', notesRouter);
+// require('./routes/users_routes')(app, passport);
+// require('./routes/notes_routes')(notesRouter);
+// app.use('/v1', notesRouter);
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
