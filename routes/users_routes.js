89e385484032284ff0815d5e97f2019e49be09c2
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-18 13:41:05 -0800

    user routes

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -3,22 +3,41 @@
 var User = require('../models/user');
 
 module.exports = function(app, passport) {
-  app.get('/api/users', passport.authenticate('basic', {session: false}), function(req, res) {
-    res.json({'jwt': req.user.generateToken(app.get('jwtSecret'))});
-  });
+app.get('/login', passport.authenticate('basic', {session: false}), function(req, res){
+      res.json({'jwt': req.user.generateToken(app.get('jwtSecret'))});
+    });
 
-  app.post('/api/users', function(req, res) {
-    User.findOne({'email': req.body.email}, function(err, user) {
+  app.post('/login', function(req, res) {
+    User.findOne({'basic.phone_number': req.body.phone_number}, function(err, user) {
       if (err) return res.status(500).send('server error');
-      if (user) return res.status(500).send('cannot create that user');
+      if (user) return res.send('user already exsits');
+      if (req.body.password !== req.body.confirmPassword)
+      return res.status(500).send('invalid password');
+});
+});
 
-      var newUser = new User();
-      newUser.basic.email = req.body.email;
-      newUser.basic.password = newUser.generateHash(req.body.password);
-      newUser.save(function(err, data) {
-        if (err) return res.status(500).send('server error');
-        res.json({'jwt': newUser.generateToken(app.get('jwtSecret'))});
-      });
+  app.get('/login/newUser', function(req, res){
+    var newUser = new User();
+    newUser.basic.name = req.body.name;
+    newUser.basic.phone_number = req.body.phone_number;
+    console.log(req.body);
+    if(req.body.password.length <= 4) return res.status(500).send('Password must be at least 5 charecters long');
+    newUser.basic.password = newUser.generateHash(req.body.password);
+    newUser.save(function(err, data) {
+      if (err) return res.status(500).send('server error');
+      res.json({'jwt': newUser.generateToken(app.get('jwtSecret'))});
     });
   });
+
+
+  //app.post('/login/newUser',function(req, res){
+
+  //});
+
+
+
+
+
+
+
 };
