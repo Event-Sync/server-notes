8d186bc3767daeac8e893539cdf613cfabe5f8bb
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-18 17:23:16 -0800

    updates test

--- test/api/iaminTest.js
+++ test/api/iaminTest.js
@@ -1,4 +1,4 @@
-process.env.MONGO_URL = 'mongodb://localhost/notes_test';
+process.env.MONGO_URL = 'mongodb://localhost/event_test';
 var chai = require('chai');
 var chaihttp = require('chai-http');
 chai.use(chaihttp);
@@ -9,7 +9,7 @@ var expect = chai.expect;
 
 describe('Create new event', function() {
   var id;
-  it('should be able to create a note', function(done) {
+  it('should be able to create a event', function(done) {
     chai.request('http://localhost:3000')
     .post('/api/newEvent')
     .send({event_name: 'Code Party',})
@@ -38,10 +38,33 @@ describe('Create new event', function() {
     .end(function(err, res){
       expect(err).to.eql(null);
       expect(res.body.participants).to.eql(3);
+      done();
     });
   });
 
-  it('should be able to destroy a note', function(done) {
+  it('should be able to update specific event', function(done){
+    chai.request('http://localhost:3000')
+    .post('/api/Event/:_id')
+    .send({event_name: 'Dance Party'})
+    .end(function(err, res){
+      expect(err).to.eql(null);
+      expect(res.body.event._id).to.eql('Dance Party!');
+      done();
+    });
+  });
+
+it('should be able to create a new user', function(done){
+  chai.request('http://localhost:3000')
+  .post('/login/newUser')
+  .send({name: "Brent", phone_numer: "5559993345", password: "abc123"})
+  .end(function(err, res){
+    expect(err).to.eql(null);
+    expect(res.status.code).to.eql(200);
+    done();
+  });
+});
+
+  it('should be able to delete and event', function(done) {
     chai.request('http://localhost:3000')
     .delete('/api/delete/' + id)
     .end(function(err, res) {
